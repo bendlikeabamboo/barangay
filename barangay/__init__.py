af6eef1fcd5231a68842b590dbb5fc1cb5cf2683
bendlikeabamboo <mbalmeo32@gmail.com>
2025-08-13 03:03:13 +0800

    Added Flat Model

--- barangay/__init__.py
+++ barangay/__init__.py
@@ -7,6 +7,8 @@ import json
 
 _BARANGAY_FILENAME = "barangay.json"
 _BARANGAY_EXTENDED_FILENAME = "barangay_extended.json"
+_BARANGAY_FLAT_FILENAME = "barangay_flat.json"
+
 
 _current_file_path = Path(__file__).resolve()
 _current_dir = _current_file_path.parent
@@ -18,3 +20,8 @@ with open(
     f"{_current_dir}/{_BARANGAY_EXTENDED_FILENAME}", encoding="utf8", mode="r"
 ) as file:
     BARANGAY_EXTENDED = json.load(file)
+
+with open(
+    f"{_current_dir}/{_BARANGAY_FLAT_FILENAME}", encoding="utf8", mode="r"
+) as file:
+    BARANGAY_FLAT = json.load(file)
